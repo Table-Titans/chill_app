f8c97040ab23532111d997ee4e7209aee1be9772
Will C. <willchanania@gmail.com>
2025-10-07 02:54:55 -0400

    added boostrap packages and crated a simple homepage

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 
 def create_app():
     app = Flask(__name__)
+    bootstrap = Bootstrap5(app) # Initialize Flask-Bootstrap
 
     # later: app.config.from_object("yourproject.config.Config")
 
